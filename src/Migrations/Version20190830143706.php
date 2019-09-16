5174cafde33ecc5e3278d2fd59a13935dba5fb26
Mykolas <mykolasvitkus@gmail.com>
2019-09-16 17:40:48 +0300

    modified migration

--- src/Migrations/Version20190830143706.php
+++ src/Migrations/Version20190830143706.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace DoctrineMigrations;
+namespace PTS\SyliusReferralPlugin\DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
